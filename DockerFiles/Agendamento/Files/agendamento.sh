797c7cf92abeb423edf2318c92ba639fb53cc62e
Kaio Goncalves Carvalho <kaio.carvalho@idealinvest.com.br>
2018-01-03 15:25:01 -0200

    Correção no diretório do backoffice

--- DockerFiles/Agendamento/Files/agendamento.sh
+++ DockerFiles/Agendamento/Files/agendamento.sh
@@ -5,7 +5,7 @@ setup_agendamento()
     docker run --rm -v $AGENDAMENTO_LOCAL/:/app kaioidealinvest/composer:php7.1 install
     cd $AGENDAMENTO_LOCAL
     cp .env.example .env
-    DIR_DOCKER=$(echo $BACKOFFICE_DOCKER | sed -e "s/\//\\\\\//g")
+    DIR_DOCKER=$(echo $BACKOFFICE_DOCKER/ | sed -e "s/\//\\\\\//g")
     sed -i -E "s/BACKOFFICE_REPOSITORY=(.*)/BACKOFFICE_REPOSITORY=$DIR_DOCKER/g" .env
     cp $BACKOFFICE_LOCAL/.env $AGENDAMENTO_LOCAL/helpers/backoffice.env.bkp
     cd $AGENDAMENTO_LOCAL

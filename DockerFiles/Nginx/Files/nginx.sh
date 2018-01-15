8f5f45402227c1ef96fb8c0d10bf963c9c45f1f1
Kaio Goncalves Carvalho <kaio.carvalho@idealinvest.com.br>
2018-01-15 12:54:08 -0200

    Melhoria no nginx

--- DockerFiles/Nginx/Files/nginx.sh
+++ DockerFiles/Nginx/Files/nginx.sh
@@ -3,39 +3,41 @@
 
 server()
 {
-    SERVER="
-        \n server {
-        \n\t listen 80;
-        \n\n\t server_name  $1;
-        \n\n\t location / {
-        \n\t\t proxy_pass http://$2/;
-        \n\t\t proxy_http_version 1.1;
-        \n\t\t proxy_set_header Upgrade \$http_upgrade;
-        \n\t\t proxy_set_header Connection 'upgrade';
-        \n\t\t proxy_set_header Host \$host;
-        \n\t\t proxy_set_header X-Real-IP  \$remote_addr;
-        \n\t\t proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        \n\t\t proxy_cache_bypass \$http_upgrade;
-        \n\t }
-        \n }
+    if isValidRepository $1; then
+        SERVER="
+            \n server {
+            \n\t listen 80;
+            \n\n\t server_name  $2;
+            \n\n\t location / {
+            \n\t\t proxy_pass http://$3/;
+            \n\t\t proxy_http_version 1.1;
+            \n\t\t proxy_set_header Upgrade \$http_upgrade;
+            \n\t\t proxy_set_header Connection 'upgrade';
+            \n\t\t proxy_set_header Host \$host;
+            \n\t\t proxy_set_header X-Real-IP  \$remote_addr;
+            \n\t\t proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            \n\t\t proxy_cache_bypass \$http_upgrade;
+            \n\t }
+            \n }
 
-        \n server {
-        \n\t listen 9000;
-        \n\n\t server_name  $1;
-        \n\n\t location / {
-        \n\t\t proxy_pass http://$2:9000/;
-        \n\t\t proxy_http_version 1.1;
-        \n\t\t proxy_set_header Upgrade \$http_upgrade;
-        \n\t\t proxy_set_header Connection 'upgrade';
-        \n\t\t proxy_set_header Host \$host;
-        \n\t\t proxy_set_header X-Real-IP  \$remote_addr;
-        \n\t\t proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        \n\t\t proxy_cache_bypass \$http_upgrade;
-        \n\t }
-        \n }
+            \n server {
+            \n\t listen 9000;
+            \n\n\t server_name  $2;
+            \n\n\t location / {
+            \n\t\t proxy_pass http://$3:9000/;
+            \n\t\t proxy_http_version 1.1;
+            \n\t\t proxy_set_header Upgrade \$http_upgrade;
+            \n\t\t proxy_set_header Connection 'upgrade';
+            \n\t\t proxy_set_header Host \$host;
+            \n\t\t proxy_set_header X-Real-IP  \$remote_addr;
+            \n\t\t proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            \n\t\t proxy_cache_bypass \$http_upgrade;
+            \n\t }
+            \n }
 
-        ";
-    echo $SERVER >> default.conf
+            ";
+        echo $SERVER >> default.conf
+    fi
 }
 
 setup_nginx()
@@ -43,13 +45,13 @@ setup_nginx()
     cd $INTEGRACAO_DIR/$NGINX_DIR
     cp default.example.conf default.conf
     chmod 777 default.conf
-    server $BACKOFFICE_URL backoffice
-    server $BACKOFFICE_API_URL backoffice
-    server $PORTALPRAVALER_URL portal_pravaler
-    server $APIAPROVACAO_URL api_aprovacao
-    server $APIAPARTADA_URL api_apartada
-    server $CREDITSCORE_URL creditscore
-    server $AGENDAMENTO_URL agendamento
+    server $BACKOFFICE_LOCAL $BACKOFFICE_URL backoffice
+    server $BACKOFFICE_LOCAL $BACKOFFICE_API_URL backoffice
+    server $PORTALPRAVALER_LOCAL $PORTALPRAVALER_URL portal_pravaler
+    server $APIAPROVACAO_LOCAL $APIAPROVACAO_URL api_aprovacao
+    server $APIAPARTADA_LOCAL $APIAPARTADA_URL api_apartada
+    server $CREDITSCORE_LOCAL $CREDITSCORE_URL creditscore
+    server $AGENDAMENTO_LOCAL $AGENDAMENTO_URL agendamento
 }
 
 

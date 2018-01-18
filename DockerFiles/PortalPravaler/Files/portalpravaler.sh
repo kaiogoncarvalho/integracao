2e07144834a5ae81be7527f05bedec4ad80482a2
Kaio Goncalves Carvalho <kaio.carvalho@idealinvest.com.br>
2018-01-18 18:08:04 -0200

    Inclusão do update no composer

--- DockerFiles/PortalPravaler/Files/portalpravaler.sh
+++ DockerFiles/PortalPravaler/Files/portalpravaler.sh
@@ -5,10 +5,15 @@ setup_portal_pravaler()
     docker run --rm -v $PORTALPRAVALER_LOCAL/:/app kaioidealinvest/composer:php7.1 install --no-scripts
     docker run --rm -v $PORTALPRAVALER_LOCAL/:/app kaioidealinvest/composer:php7.1 update --no-scripts
     docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/analytics:/app kaioidealinvest/composer:php7.1 install
+    docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/analytics:/app kaioidealinvest/composer:php7.1 update
     docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/plugins:/app kaioidealinvest/composer:php7.1 install
+    docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/plugins:/app kaioidealinvest/composer:php7.1 update
     docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/pravaler-backoffice:/app kaioidealinvest/composer:php7.1 install
+    docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/pravaler-backoffice:/app kaioidealinvest/composer:php7.1 update
     docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/proposal:/app kaioidealinvest/composer:php7.1 install
+    docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/proposal:/app kaioidealinvest/composer:php7.1 update
     docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/marketplace:/app kaioidealinvest/composer:php7.1 install
+    docker run --rm -v $PORTALPRAVALER_LOCAL/workbench/portal/marketplace:/app kaioidealinvest/composer:php7.1 update
     cd $PORTALPRAVALER_LOCAL
     chmod -R 777 app/storage
     chmod -R 777 vendor

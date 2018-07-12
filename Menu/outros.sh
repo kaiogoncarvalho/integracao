c895bdbac8222c08b7ffce42d61c8d028f907500
Kaio Goncalves Carvalho <kaio.carvalho@pravaler.com.br>
2018-07-12 17:49:25 -0300

    Correção na ordem de instalação para manter ordem alfabetica

--- Menu/outros.sh
+++ Menu/outros.sh
@@ -61,12 +61,13 @@ outros() {
         printLine "5  - CDN"
         printLine "6  - CreditScore"
         printLine "7  - FTP Risco e Cobrança"
-        printLine "8  - Nova Proposta Backend"
-        printLine "9  - Nova Proposta Frontend"
-        printLine "10 - Portal Pravaler"
-        printLine "11 - Retorno Mec"
-        printLine "12 - Seguros"
-        printLine "13 - Marketplace"
+        printLine "8  - Marketplace"
+        printLine "9  - Nova Proposta Backend"
+        printLine "10 - Nova Proposta Frontend"
+        printLine "11 - Portal Pravaler"
+        printLine "12 - Retorno Mec"
+        printLine "13 - Seguros"
+
         printLine "0  - Voltar" "branco" "negrito"
         printInBar "s - Sair" "vermelho"
         read -p "| Informe a opção desejada >_ " OPTION
@@ -104,18 +105,19 @@ outros() {
             reloadEnv
             setup_ftp_risco_cobranca
           ;;
-          8) installSystem "Nova Proposta Backend" "NOVAPROPOSTA_BACKEND" "setup_nova_proposta_backend"
+          8) installSystem "Marketplace Api" "MARKETPLACE_API" "setup_marketplace_api"
           ;;
-          9) installSystem "Nova Proposta Frontend" "NOVAPROPOSTA_FRONTEND" "setup_nova_proposta_frontend"
+          9) installSystem "Nova Proposta Backend" "NOVAPROPOSTA_BACKEND" "setup_nova_proposta_backend"
           ;;
-          10) installSystem "Portal Pravaler" "PORTALPRAVALER" "setup_portal_pravaler"
+          10) installSystem "Nova Proposta Frontend" "NOVAPROPOSTA_FRONTEND" "setup_nova_proposta_frontend"
           ;;
-          11) installSystem "Retorno Mec" "RETORNO_MEC" "retorno_mec"
+          11) installSystem "Portal Pravaler" "PORTALPRAVALER" "setup_portal_pravaler"
           ;;
-          12) installSystem "Seguros" "SEGUROS" "setup_seguros"
+          12) installSystem "Retorno Mec" "RETORNO_MEC" "retorno_mec"
           ;;
-          13) installSystem "Marketplace Api" "MARKETPLACE_API" "setup_marketplace_api"
+          13) installSystem "Seguros" "SEGUROS" "setup_seguros"
           ;;
+
           *) clear
             printInBar "Opção inválida!" "vermelho"
           ;;

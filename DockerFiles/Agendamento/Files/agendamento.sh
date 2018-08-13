3ad346a607a99287f22900afab47c8b170f5ddcb
Kaio Goncalves Carvalho <kaio.carvalho@pravaler.com.br>
2018-08-13 09:56:45 -0300

    Nome do Servidor

--- DockerFiles/Agendamento/Files/agendamento.sh
+++ DockerFiles/Agendamento/Files/agendamento.sh
@@ -2,15 +2,13 @@
 
 setup_agendamento()
 {
-    composerConfig $1
     cd $1
+    composerConfig $1
 
     configInitialEnv '.env.example'
 
-    regexFile 'BACKOFFICE_REPOSITORY=' $BACKOFFICE_DOCKER
-    cp $BACKOFFICE_LOCAL/.env $1/helpers/backoffice.env.bkp
+    regexFile 'APP_URL=' "$AGENDAMENTO_URL"
 
-    msgConfig "Criando diretórios: "
     cd $1
     if [ -d "xdebug-profile-logs" ]
     then
@@ -19,11 +17,28 @@ setup_agendamento()
         mkdir xdebug-profile-logs
         msgConfigItem "Diretório $(pwd)/xdebug-profile-logs foi criado."
     fi
+
+     if [ -d '/var/data-mysql' ]
+    then
+        msgConfigItem "Diretório $(pwd)/xdebug-profile-logs já existe."
+    else
+        mkdir /var/data-mysql
+        msgConfigItem "Diretório $(pwd)/xdebug-profile-logs foi criado."
+    fi
     chmod 777 -R $1
 
    dockerComposeUp $AGENDAMENTO_CONTAINER
 
    configHost $AGENDAMENTO_CONTAINER $AGENDAMENTO_URL
+
+   msgConfig "Executando php artisan key:generate: "
+   docker exec -ti nova_proposta_backend php "$NOVAPROPOSTA_BACKEND_DOCKER/artisan" key:generate
+
+   msgConfig "Executando php artisan migrate: "
+   docker exec -ti nova_proposta_backend php "$NOVAPROPOSTA_BACKEND_DOCKER/artisan" migrate
+
+   msgConfig "Executando php artisan db:seed: "
+   docker exec -ti nova_proposta_backend php "$NOVAPROPOSTA_BACKEND_DOCKER/artisan" db:seed
 
 
 }

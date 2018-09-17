b6c1cd53fe74e10d5619ae4b0c9da0c213d508d7
Kaio Goncalves Carvalho <kaio.carvalho@pravaler.com.br>
2018-09-17 20:29:06 -0300

    funcionar https no servidor

--- DockerFiles/Nginx/Files/nginx.sh
+++ DockerFiles/Nginx/Files/nginx.sh
@@ -41,7 +41,7 @@ server()
             \n server {
             \n\t listen 443 ssl;
             \n\t ssl_certificate /etc/ssl/phpit.pem;
-            \n\t ssl_certificate_key /etc/ssl/phpit.key;s
+            \n\t ssl_certificate_key /etc/ssl/phpit.key;
             \n\t server_name $URL;
             \n\t return 301 http://$URL;
             \n}
